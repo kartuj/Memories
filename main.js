8aa12e09c4637c211a8479e8738aefddd50a2ae7
kartuj <90568181+kartuj@users.noreply.github.com>
2021-09-12 21:42:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var i = [
+"_04-35-15_18092009196.jpg",
+"_12-42-16_ish.JPG",
+"_14-23-02_DSC00595 [1].JPG",
+"_16-18-14_07092009147.jpg",
+"DSC01303.JPG",
+"DSC01463.JPG",
+"DSC01552.JPG",
+"DSC02070.JPG",
+"DSC02652.JPG",
+"DSC03412.JPG",
+"DSC03437.JPG",
+"IMG_20200127_134550.jpg",
+"shribhagwan ji n family 024.JPG"];
+
+var e = 0 
+function change() 
+    {document.getElementById("img").src= i[e];e++ ;
+                       if (e==13)
+   
+     e=0;
+     }
